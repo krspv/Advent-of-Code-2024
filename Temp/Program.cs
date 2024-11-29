db55c4be8add6587d86b9576cadc0836ad496c94
krspv <31394089+krspv@users.noreply.github.com>
2024-11-30 00:40:38 +0100

    Updated .gitignore

--- Temp/Program.cs
+++ Temp/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Stopwatch stopwatch = new();
+
+stopwatch.Start();
+Thread.Sleep(100);
+stopwatch.Stop();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nПрв дел:");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("  Решение");
+Console.ResetColor();
+Console.WriteLine($"Време на извршување: {stopwatch.ElapsedMilliseconds}ms");
+
+
+stopwatch.Restart();
+Thread.Sleep(300);
+stopwatch.Stop();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\n\nВтор дел:");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("  Решение");
+Console.ResetColor();
+Console.WriteLine($"Време на извршување: {stopwatch.ElapsedMilliseconds}ms");
+
+Console.WriteLine("\n\n\n\n");
