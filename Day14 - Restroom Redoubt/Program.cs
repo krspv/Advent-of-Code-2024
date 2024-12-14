f5b9cf4ae6fdacc9f1e691fb136dc382ec09b8d9
krspv <31394089+krspv@users.noreply.github.com>
2024-12-14 17:20:12 +0100

    Improved Day14 solution

--- Day14 - Restroom Redoubt/Program.cs
+++ Day14 - Restroom Redoubt/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Day 14 - Restroom Redoubt
 using Helpers;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -9,14 +8,13 @@ Stopwatch stopwatch = new();
 PrintHelper.ПечатиНаслов(14, "Restroom Redoubt");
 
 string fileName = Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName, "input.txt");
-string fileOut = Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName, "output.txt");
 
 
 
 
 
 // Read the input
-List<Robot> robots = [], robotsForPart2 = [];
+List<Robot> robots = [], robotsP2 = [];
 foreach (string line in File.ReadAllLines(fileName)) {
   int[] values = Regex.Matches(line, @"^p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)$")[0]
     .Groups.Cast<Group>().Skip(1).Select(g => Int32.Parse(g.Value)).ToArray();
@@ -24,12 +22,12 @@ foreach (string line in File.ReadAllLines(fileName)) {
     Pos = (values[0], values[1]),
     Vel = (values[2], values[3]),
   });
-  robotsForPart2.Add(new Robot() {
+  robotsP2.Add(new Robot() {
     Pos = (values[0], values[1]),
     Vel = (values[2], values[3]),
   });
 }
-(long x, long y) maxPos = (robots.Max(r => 1 + r.Pos.x), robots.Max(r => 1 + r.Pos.y));
+(int x, int y) maxPos = (robots.Max(r => 1 + r.Pos.x), robots.Max(r => 1 + r.Pos.y));
 
 
 
@@ -62,29 +60,20 @@ PrintHelper.ПечатиПрвДел(stopwatch.ElapsedMilliseconds, nProduct);
 stopwatch.Restart();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Part 2
-long nSteps = 98;
-foreach (Robot robot in robotsForPart2)
-  robot.Move(98, maxPos);
+// It seems that the christmas tree happens when no robots overlap
+int nSteps = 0;
 
 while (true) {
-  bool[,] mat = new bool[maxPos.x, maxPos.y];
-  foreach (Robot robot in robotsForPart2)
-    mat[robot.Pos.x, robot.Pos.y] = true;
-  using (StreamWriter writer = new StreamWriter(fileOut)) {
-    writer.WriteLine(nSteps);
-    for (int y = 0; y < maxPos.y; ++y) {
-      for (int x = 0; x < maxPos.x; ++x) {
-        writer.Write(mat[x, y] ? '#' : '.');
-      }
-      writer.WriteLine();
-    }
+  bool bOverlap = false;
+  bool[,] posOverlap = new bool[maxPos.x, maxPos.y];
+  foreach (Robot robot in robotsP2) {
+    robot.Move(1, maxPos);
+    if (posOverlap[robot.Pos.x, robot.Pos.y]) bOverlap = true;
+    posOverlap[robot.Pos.x, robot.Pos.y] = true;
   }
 
-  if (nSteps == 7572) break;
-
-  nSteps += maxPos.x;
-  foreach (Robot robot in robotsForPart2)
-    robot.Move(maxPos.x, maxPos);
+  nSteps++;
+  if (!bOverlap) break;
 }
 // Part 2
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -105,11 +94,11 @@ PrintHelper.ПечатиЕлкаЗаКрај();
 // Types
 [DebuggerDisplay("Pos: ({Pos.x}, {Pos.y}), Vel: ({Vel.x}, {Vel.y})")]
 class Robot {
-  public (long x, long y) Pos { get; set; }
-  public (long x, long y) Vel { get; set; }
+  public (int x, int y) Pos { get; set; }
+  public (int x, int y) Vel { get; set; }
 
-  public void Move(long nSeconds, (long x, long y) mxPos) {
-    (long x, long y) nextPos = ((Pos.x + Vel.x * nSeconds) % mxPos.x, (Pos.y + Vel.y * nSeconds) % mxPos.y);
+  public void Move(int nSeconds, (int x, int y) mxPos) {
+    (int x, int y) nextPos = ((Pos.x + Vel.x * nSeconds) % mxPos.x, (Pos.y + Vel.y * nSeconds) % mxPos.y);
     if (nextPos.x < 0)
       nextPos.x += mxPos.x;
     if (nextPos.y < 0)
@@ -117,7 +106,7 @@ class Robot {
     Pos = nextPos;
   }
 
-  public KQuadrant Quadrant((long x, long y) mxPos) => Pos switch {
+  public KQuadrant Quadrant((int x, int y) mxPos) => Pos switch {
     var (x, y) when (x < mxPos.x / 2 && y < mxPos.y / 2) => KQuadrant.TopLeft,
     var (x, y) when (x > mxPos.x / 2 && y < mxPos.y / 2) => KQuadrant.TopRight,
     var (x, y) when (x < mxPos.x / 2 && y > mxPos.y / 2) => KQuadrant.BottomLeft,
