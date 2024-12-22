3bc2bfaa783534497718cddd454ca8b52d6ccb17
krspv <31394089+krspv@users.noreply.github.com>
2024-12-22 12:13:44 +0100

    Update Program.cs

--- Day22 - Monkey Market/Program.cs
+++ Day22 - Monkey Market/Program.cs
@@ -53,7 +53,7 @@ PrintHelper.ПечатиПрвДел(stopwatch.ElapsedMilliseconds, nSum);
 stopwatch.Restart();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Part 2
-Dictionary<int, int>dict = [];  // <sequence, number> - The number when this sequence appears for the first time
+Dictionary<int, int> dict = [];  // <sequence, number> - The number when this sequence appears for the first time
 
 foreach (long nNum in secrets) {
   long nTemp = nNum;
@@ -64,20 +64,21 @@ foreach (long nNum in secrets) {
     long n = Evolve(nTemp);
     int n1 = (int)(n % 10);
     int x1 = (int)(nTemp % 10);
+
     q.Enqueue(n1 - x1 + 9); // 0..18
     if (q.Count == 4) {
       if (n1 > 0) {
         List<int> lst = [.. q];
         int val20 = lst[0] * 8000 + lst[1] * 400 + lst[2] * 20 + lst[3];
         if (!used.Contains(val20)) {
-          if (!dict.ContainsKey(val20))
-            dict[val20] = 0;
+          if (!dict.ContainsKey(val20)) dict[val20] = 0;
           dict[val20] += n1;
           used.Add(val20);
         }
       }
       q.Dequeue();
     }
+
     nTemp = n;
   }
 }
